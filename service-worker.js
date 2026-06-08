24e5f8592c75f5ab3a61d6d08a8ea9c1d6b324c5
mtarabet72 <dreamzara76@gmail.com>
2026-06-08 17:24:57 +0100

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,6 +0,0 @@
-
-const CACHE='hm-firebase-v1';
-const ASSETS=['/','/index.html','/manifest.json','/icons/icon-192.png','/icons/icon-512.png'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))))});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).catch(()=>caches.match('./index.html'))))});
